39d37682fb06c744c688fd9714728dd3c48aed50
SimaEnaga <vharugun9093@gmail.com>
2024-07-08 21:14:56 +0900

    Set up VSCode dev environment

---
+++ Source/SNPorter_CPP/Base/CargoGameModeBase.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CargoGameModeBase.h"
+
+void ACargoGameModeBase::SummitCargoToPos(TSoftObjectPtr<AActor> TargetActor, FVector TargetPos)
+{
+
+}
