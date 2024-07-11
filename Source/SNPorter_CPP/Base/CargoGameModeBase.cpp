76a84e0e42a4036cc808aae5975bb58907099309
SimaEnaga <vharugun9093@gmail.com>
2024-07-11 20:53:23 +0900

    Revert "Set up VSCode dev environment"
    This reverts commit 39d37682fb06c744c688fd9714728dd3c48aed50.

--- Source/SNPorter_CPP/Base/CargoGameModeBase.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "CargoGameModeBase.h"
-
-void ACargoGameModeBase::SummitCargoToPos(TSoftObjectPtr<AActor> TargetActor, FVector TargetPos)
-{
-
-}
