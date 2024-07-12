c1993dbaa713a399d7dde355fc2ad60545dd5f20
SimaEnaga <vharugun9093@gmail.com>
2024-07-12 21:35:49 +0900

    Add CargoBaseActor class

---
+++ Source/SNPorter_CPP/Base/SNCargoBase.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SNCargoBase.generated.h"
+
+UCLASS()
+class SNPORTER_CPP_API ASNCargoBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASNCargoBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
