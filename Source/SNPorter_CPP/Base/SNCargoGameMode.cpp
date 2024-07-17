047377f4337b8eba78a95c0bd34b06fdac3e9fca
SimaEnaga <vharugun9093@gmail.com>
2024-07-17 21:12:52 +0900

    Migration cpp - Cargo GameMode Update

--- Source/SNPorter_CPP/Base/SNCargoGameMode.cpp
+++ Source/SNPorter_CPP/Base/SNCargoGameMode.cpp
@@ -3,3 +3,16 @@
 
 #include "../Base/SNCargoGameMode.h"
 
+void ASNCargoGameMode::BeginPlay()
+{
+    for (int x = 0; x < CargoSize.X; x++)
+    {
+        for (int y = 0; y < CargoSize.Y; y++)
+        {
+            for (int z = 0; z < CargoSize.Z; z++)
+            {
+                LoadedCargoInfo.Add(FVector(x, y, z), -1);
+            }
+        }
+    }
+}
--- Source/SNPorter_CPP/Base/SNCargoGameMode.h
+++ Source/SNPorter_CPP/Base/SNCargoGameMode.h
@@ -15,4 +15,14 @@ class SNPORTER_CPP_API ASNCargoGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SNPorter")
+	FVector CargoSize;
+
+	UPROPERTY(BlueprintReadWrite, Category = "SNPorter")
+	TMap<FVector, int> LoadedCargoInfo;
 };
